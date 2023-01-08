6822d7e3ae8701cf41a4185ef15dc8cca1edef97
SaaFomenko <s.fomenko@list.ru>
2023-01-08 13:37:53 +0300

    Finish demo cmake projectt.

---
+++ CMakeFiles/hello_cmake.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cmake.out.dir/source.cpp.o"
+  "hello_cmake.out.pdb"
+  "hello_cmake.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cmake.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+main()
+{
+	std::cout << "Hello, User!" << std::endl;
+
+	return 0;
+}
